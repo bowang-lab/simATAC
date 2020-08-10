b47f49dde71a6d5721b07c55c564b979675f548b
Zeinab <z.navidi90@gmail.com>
2020-08-10 11:53:56 -0400

    Add simATACCompare function.

--- R/ExtraFunctions.R
+++ R/ExtraFunctions.R
@@ -251,3 +251,164 @@ simATACgetCellByRegion <- function(sim, file.bed){
 
   return(count)
 }
+
+
+#' Compare the main parameters of simATAC's simulated bin by cell matrix to real
+#' matrix, including library size, bin mean, non-zero cell proportion, and the
+#' relation between bin means and non-zero cell proportions.
+#'
+#' @param sim Simulated bin by cell sparse matrix.
+#' @param real Real bin by cell sparse matrix.
+#' @param address Folder address to save plots.
+#' @param name Name of the data.
+#'
+#' @return
+#'
+#' @importFrom ggpubr ggboxplot 
+#' @importFrom ggplot2 ggplot ggtitle geom_point theme theme_bw xlab ylab element_blank element_text ylim xlim aes
+#' @importFrom grDevices png dev.off
+#' @importFrom utils write.table
+#' @importFrom stats cor
+#' @export
+#'
+simATACCompare <- function(sim, real, address, name){
+
+  # Library size
+  sim.lib.size <- log2(colSums(sim)+1)
+  real.lib.size <- log2(colSums(real)+1)
+
+  sim.lib.size.df <- data.frame(group = "Simulated", value = sim.lib.size)
+  real.lib.size.df <- data.frame(group = "Real", value = real.lib.size)
+
+  df <- rbind(real.lib.size.df, sim.lib.size.df)
+  p <- ggboxplot(df, x = "group", y = "value",
+                 color = "group", palette =c("#00AFBB", "#FC4E07"),
+                 add = "jitter", shape = "group") + ggtitle(name) +
+    theme(
+      plot.title = element_text(size=24, face="bold.italic", hjust = 0.5),
+      axis.title.x = element_blank(),
+      axis.title.y = element_text(size=24),
+      axis.text.x = element_text(size = 24),
+      axis.text.y = element_text(size = 24)
+    ) + theme(legend.position = "none")
+  
+  png(paste(address, "/Library_size_boxplot.png", sep = ""))
+  print(p+ylab("log2(library size)"))
+  dev.off()
+  
+  
+  # Bin sparsity (proportion of zero entries in each bin)
+  real.bin.sparsity <- rowSums(real == 0)/ncol(real)
+  sim.bin.sparsity <- rowSums(sim == 0)/ncol(sim)
+  
+  real.bin.sparsity.df <- data.frame(group = "Real", value = real.bin.sparsity)
+  sim.bin.sparsity.df <- data.frame(group = "Simulated", value =sim.bin.sparsity)
+  
+  df <- rbind(real.bin.sparsity.df, sim.bin.sparsity.df)
+  p <- ggboxplot(df, x = "group", y = "value",
+                 color = "group", palette =c("red", "blue"),
+                 add = "jitter", shape = "group") + ggtitle(name) +
+    theme(
+      plot.title = element_text(size=24, face="bold.italic", hjust = 0.5),
+      axis.title.x = element_blank(),
+      axis.title.y = element_text(size=24),
+      axis.text.x = element_text(size = 24),
+      axis.text.y = element_text(size = 24)
+    ) + theme(legend.position = "none")
+  
+  png(paste(address, "/Bin_sparsity_boxplot.png", sep = ""))
+  print(p+ylab("Bin sparsity")+ xlab(NULL))
+  dev.off()
+  
+  
+  # Cell sparsity (proportion of zero entries in each cell)
+  real.cell.sparsity <- colSums(real == 0)/nrow(real)
+  sim.cell.sparsity <- colSums(sim == 0)/nrow(sim)
+  
+  real.cell.sparsity.df <- data.frame(group = "Real", value = real.cell.sparsity)
+  sim.cell.sparsity.df <- data.frame(group = "Simulated", value =sim.cell.sparsity)
+  
+  df <- rbind(real.cell.sparsity.df, sim.cell.sparsity.df)
+  p <- ggboxplot(df, x = "group", y = "value",
+                 color = "group", palette =c("#CC79A7", "#009E73"),
+                 add = "jitter", shape = "group") + ggtitle(name) +
+    theme(
+      plot.title = element_text(size=24, face="bold.italic", hjust = 0.5),
+      axis.title.x = element_blank(),
+      axis.title.y = element_text(size=24),
+      axis.text.x = element_text(size = 24),
+      axis.text.y = element_text(size = 24)
+    ) + theme(legend.position = "none")
+  
+  png(paste(address, "/Cell_sparsity_boxplot.png", sep = ""))
+  print(p+ylab("Cell sparsity")+ xlab(NULL))
+  dev.off()
+  
+  
+  # Relation between bin means and non-zero cell proportions.
+  sim.bin.mean <- rowSums(sim)/ncol(sim)
+  real.bin.mean <- rowSums(real)/ncol(real)
+
+  sim.nzp <- rowSums(sim != 0)/ncol(sim)
+  real.nzp <- rowSums(real != 0)/ncol(real)
+  
+  max <- max(c(real.bin.mean[which(real.nzp <= 0.8)], sim.bin.mean[which(real.nzp <= 0.8)]))
+  
+  # Plot original non-zero cell proportion and bin mean scatter plot (including bins with non-zero proportion <= 0.8 in real data)
+  df <- data.frame(nzp = real.nzp[which(real.nzp<=0.8)], mean = real.bin.mean[which(real.nzp<=0.8)])
+  p <- ggplot(df, aes(x = nzp, y = mean)) +
+    geom_point() +
+    ggtitle(paste('Real', name, sep = " ")) +
+    ylim(0, max) + xlim(0, 0.8) +
+    theme_bw() +
+    theme(
+      plot.title = element_text(size=24, face="bold.italic", hjust = 0.5),
+      axis.title.x = element_text(size=24),
+      axis.title.y = element_text(size=24),
+      axis.text.x = element_text(size = 24),
+      axis.text.y = element_text(size = 24)
+    )
+  
+  png(paste(address, "/Real_bin_mean_and_nonzero_proportion.png", sep = ""))
+  print(p + xlab("Bin non-zero proportion") + ylab("Bin mean"))
+  dev.off()
+  
+  
+  # Plot simulated non-zero cell proportion and bin mean scatter plot (including bins with non-zero proportion <= 0.8 in real data)
+  df <- data.frame(nzp = sim.nzp[which(sim.nzp<=0.8)], mean = sim.bin.mean[which(sim.nzp<=0.8)])
+  p <- ggplot(df, aes(x = nzp, y = mean)) +
+    geom_point() +
+    ggtitle(paste('Simulated', name, sep = " ")) +
+    ylim(0, max) + xlim(0, 0.8) +
+    theme_bw() +
+    theme(
+      plot.title = element_text(size=24, face="bold.italic", hjust = 0.5),
+      axis.title.x = element_text(size=24),
+      axis.title.y = element_text(size=24),
+      axis.text.x = element_text(size = 24),
+      axis.text.y = element_text(size = 24)
+    )
+
+  png(paste(address, "/Simulated_bin_mean_and_nonzero_proportion.png", sep = ""))
+  print(p + xlab("Bin non-zero proportion") + ylab("Bin mean"))
+  dev.off()
+  
+  
+  if (dim(sim)[2] == dim(real)[2]){
+    # Save Pearson correlation between bin means and non-zero cell proportion parameters.
+    write.table(c(paste('Name', 'Parameter', 'Pearson_correlation', sep = "    ")),
+                  file = paste(address, "/", name, "_correlation.txt", sep = ""),
+                  append = TRUE, sep = " ", col.names = FALSE, quote = FALSE, row.names = FALSE)
+
+    # Save the bin mean Pearson correlation between real and simulated datasets (including bins with non-zero proportion <= 0.8 in real data).
+    write.table(c(paste(name, "BinMeanCor<=0.8", as.character(cor(real.bin.mean[which(real.nzp <= 0.8)], sim.bin.mean[which(real.nzp <= 0.8)], method = c("pearson"))), sep = "    ")),
+                file = paste(address, "/", name, "_correlation.txt", sep = ""),
+                append = TRUE, sep = " ", col.names = FALSE, quote = FALSE, row.names = FALSE)
+
+    # Save the non-zero cell proportion Pearson correlation between real and simulated datasets.
+    write.table(c(paste(name, "NZeroPropCor", as.character(cor(real.nzp, sim.nzp, method = c("pearson"))), sep = "    ")),
+                file = paste(address, "/", name, "_correlation.txt", sep = ""),
+                append = TRUE, sep = " ", col.names = FALSE, quote = FALSE, row.names = FALSE) 
+  }
+
+}
